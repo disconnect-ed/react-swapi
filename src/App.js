307e79e26550fc9a643446264f93a7de4566d2fb
disconnect-ed <ruslan199812@gmail.com>
2020-10-22 22:17:12 +0300

    created a useFetch hook for server requests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
 function App() {
+
   return (
     <div className="app min-vh-100 w-100 position-relative">
       <Container fluid>
