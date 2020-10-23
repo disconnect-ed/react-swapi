76e9b32847b46dd9c4c59da8729a058c52942bb6
disconnect-ed <ruslan199812@gmail.com>
2020-10-23 15:54:57 +0300

    fixed import of Switch to App.js, added processing of non-existent page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import './App.css';
 import Container from "react-bootstrap/Container";
-import Switch from "react-bootstrap/Switch";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import {MainPageContainer} from "./pages/MainPage/MainPageContainer";
 import {PlanetPageContainer} from "./pages/PlanetPage/PlanetPageContainer";
+import {NotFound} from "./components/NotFound/NotFound";
 
 function App() {
-
-  return (
-    <div className="app min-vh-100 w-100 position-relative">
-      <Container>
-        <Switch>
-          <Route exact
-                 path='/'
-                 render={() => <MainPageContainer/>}
-          />
-            <Route exact
-                   path='/planets/:id'
-                   render={() => <PlanetPageContainer/>}
-            />
-        </Switch>
-
-
-      </Container>
-
-    </div>
-  );
+    return (
+        <div className="app min-vh-100 w-100 position-relative">
+            <Container>
+                <Switch>
+                    <Route exact
+                           path='/'
+                           render={() => <MainPageContainer/>}
+                    />
+                    <Route exact
+                           path='/planets/:id'
+                           render={() => <PlanetPageContainer/>}
+                    />
+                    <Route exact
+                           path='*'
+                           render={() => <NotFound text='Page not found :('/>}
+                    />
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
